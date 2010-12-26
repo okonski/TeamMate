24a25c97bf145e7c69bbd2bcf235398cccfb1be0
Jakub Okoński <kuba@okonski.org>
2010-12-26 16:21:21 +0100

    Add link to entries.

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,5 +2,6 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :login, 'Login', new_user_session_path
     primary.item :login, 'Logout', destroy_user_session_path
+    primary.item :entries, 'Entries', entries_path
   end
 end

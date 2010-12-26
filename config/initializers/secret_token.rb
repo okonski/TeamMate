03e67c40e2e4d70514238cd4b1328107efe6ebaf
Jakub Okoński <kuba@okonski.org>
2010-12-26 13:59:34 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClanLeopard::Application.config.secret_token = 'c35debf58cc1242ab03770d042903a996408b205f10b9fc34156695a7dc80fb72c7498b12076b63cb4933f78472fc3b717df713c18aaeedfd83e3e8eb657665a'

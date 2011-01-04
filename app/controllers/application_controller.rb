21081b7aae8cfd59a4d29e2a14017892b286f97f
Jakub Okoński <kuba@okonski.org>
2011-01-04 20:11:21 +0100

    Enable XHR Ajax requests and turn off layouts for it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
   theme "simple"
+  layout proc{ |c| c.request.xhr? ? false : "application" }
 
   protect_from_forgery
 end

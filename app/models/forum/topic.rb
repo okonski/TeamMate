a7bf9960db26f5f6ee29ce49dcc9f03e0634a205
Jakub Okoński <kuba@okonski.org>
2011-01-25 10:07:28 +0100

    Validations of Forum::Topic, working views in forum/ namespace.

--- app/models/forum/topic.rb
+++ app/models/forum/topic.rb
@@ -2,4 +2,6 @@ class Forum::Topic < ActiveRecord::Base
   belongs_to :category, :class_name => 'Forum::Category'
   belongs_to :author, :class_name => 'User'
   has_many :comments, :as => :commentable
+  
+  validates_presence_of :category, :author, :title
 end

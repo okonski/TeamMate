4bfa8018269298b9deabb1f4f59124cd881119b3
Piotrek Okoński <piotrek@okonski.org>
2011-02-17 21:57:14 +0100

    Change main manu

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,11 +1,17 @@
 SimpleNavigation::Configuration.run do |navigation|
+  
   navigation.items do |primary|
+    primary.dom_id = "nav"
+    primary.item :home, "Home", root_url
+    primary.item :forum, 'Forum', [:forum, :categories]
+    primary.item :members, 'Roster', [:users]
+    primary.item :servers, 'Servers', [:servers]
     primary.item :login, 'Login', [:new, :user_session]
     primary.item :login, 'Logout', [:destroy, :user_session]
     primary.item :entries, 'Entries', [:entries]
-    primary.item :servers, 'Servers', [:servers]
-    primary.item :members, 'Members', [:users]
+    
+    
     primary.item :messages, 'Messages', [:messages] if current_user
-    primary.item :forum, 'Forum', [:forum, :categories]
+    
   end
 end
